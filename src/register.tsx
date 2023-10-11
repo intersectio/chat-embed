4b4a12f8647698cd287faf1b7f635b4dae1257a8
dpayLuna <athenax0708@gmail.com>
2023-10-12 02:21:23 +0800

    upload from local

---
+++ src/register.tsx
@@ -0,0 +1,11 @@
+import { customElement } from 'solid-element'
+import { defaultBotProps } from './constants'
+import { Bubble } from './features/bubble'
+import { Full } from './features/full'
+
+export const registerWebComponents = () => {
+    if (typeof window === 'undefined') return
+    // @ts-expect-error element incorect type
+    customElement('cubieai-fullchatbot', defaultBotProps, Full)
+    customElement('cubieai-chatbot', defaultBotProps, Bubble)
+}
